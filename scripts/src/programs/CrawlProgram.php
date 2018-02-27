46e067900f46078118a73b666c7eb53a9a71de4c
Marco Hernandez <marcohern@gmail.com>
2018-02-27 12:13:10 -0500

    runs

--- scripts/src/programs/CrawlProgram.php
+++ scripts/src/programs/CrawlProgram.php
@@ -4,8 +4,28 @@ inc('/src/logging/CmdLogger.php');
 inc('/src/programs/Program.php');
 
 class CrawlProgram extends Program {
+	private $campaignCodes = [
+		'ALK-PS4-GAMES',
+		'AKO-PS4-GAMES',
+		'KTR-PS4-GAMES',
+		'FLB-PS4-GAMES',
+		'EXT-PS4-GAMES'
+	];
+
 	protected function execute() {
-		$this->log("Hello World");
+		$n = count($this->campaignCodes);
+		$rand = rand(0,$n-1);
+		$code = $this->campaignCodes[$rand];
+
+		$campaign = $this->campaigns->first(['code' => $code]);
+
+
+		$class = $campaign->executor;
+		inc("/src/executors/$class.php");
+		$this->log($code);
+		$exe = new $class($this->db, $this->logger);
+		$exe->init($campaign);
+		$exe->run();
 	}
 }
 
